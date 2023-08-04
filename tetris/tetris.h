47c3458617ac613323408da2815127fe40948a70
Saphal Rimal <078bct078.saphal@pcampus.edu.np>
2023-08-04 18:39:10 +0545

    Formatted

--- tetris/tetris.h
+++ tetris/tetris.h
@@ -10,11 +10,13 @@
 class Tetris
 {
 
-    public:
-    Tetris(){
+public:
+    Tetris()
+    {
 
     }
-    ~Tetris(){
+    ~Tetris()
+    {
 
     }
 
@@ -26,7 +28,7 @@ class Tetris
 
     bool isRunning()
     {
-    return running;
+        return running;
     }
 
     bool isvalid();
@@ -44,41 +46,47 @@ class Tetris
     void updateScore();
     int nextT;
 
-    private:
-        enum {ScreenW = 864, ScreenH = 800 };
-        enum {BlockW = 36, BlockH = 36};
-        enum {Lines = 20, Cols = 24};
-        SDL_Window* window = NULL;
-	 SDL_Renderer* render = NULL;
-     SDL_Texture* background = NULL, * blocks = NULL, *text=NULL;
-     SDL_Rect srcR = {0,0, BlockW, BlockH}, destR={0,0, BlockW, BlockH};
-     SDL_Rect textRect;
-     TTF_Font *font = TTF_OpenFont("Font/Tetris.ttf",20);
-     SDL_Color textColor = {0,0,0};
-     bool running = false;
-     int field[Lines][Cols]={0};
-     static const int figures[][4];
-     struct Point
-     {
-         int x,y;
-     } items[4],backup[4], items2[4], backup2[4];
-     int color =1,color2=2;
-     int dx =0;
-     int dx2=0;
-     bool rotate =false;
-     bool rotate2 = false;
-     unsigned int delay = 1000,delay2=1000;
-     int player1Score = 0; // Score for player 1
-     int player2Score = 0; // Score for player 2
-     int linesCleared = 0;
+private:
+    enum {ScreenW = 864, ScreenH = 800 };
+    enum {BlockW = 36, BlockH = 36};
+    enum {Lines = 20, Cols = 24};
+    SDL_Window* window = NULL;
+    SDL_Renderer* render = NULL;
+    SDL_Texture* background = NULL, * blocks = NULL, *text=NULL;
+    SDL_Rect srcR = {0,0, BlockW, BlockH}, destR= {0,0, BlockW, BlockH};
+    SDL_Rect textRect;
+    TTF_Font *font = TTF_OpenFont("Font/Tetris.ttf",20);
+    SDL_Color textColor = {0,0,0};
+    bool running = false;
+    int field[Lines][Cols]= {0};
+    static const int figures[][4];
+    struct Point
+    {
+        int x,y;
+    } items[4],backup[4], items2[4], backup2[4];
+    int color =1,color2=2;
+    int dx =0;
+    int dx2=0;
+    bool rotate =false;
+    bool rotate2 = false;
+    unsigned int delay=1000,delay2=1000;
+    unsigned int tempDelay=1000, tempDelay2=1000;
+    int player1Score = 0; // Score for player 1
+    int player2Score = 0; // Score for player 2
+    int linesCleared = 0;
     int linesCleared2 = 0;
+    int player1Level=0;
+    int player2Level=0;
 
-     void increasePlayer1Score(int linesCleared);
-     void increasePlayer2Score(int linesCleared);
-     void drawScores();
-     Uint32 startTime=0, currentTime =0,currentTime2=0,startTime2=0;
+    void increasePlayer1Score(int linesCleared);
+    void increasePlayer2Score(int linesCleared);
+    void increasePlayer1Level(int linesCleared);
+    void increasePlayer2Level(int linesCleared);
+    void drawScores();
+    Uint32 startTime=0, currentTime =0,currentTime2=0,startTime2=0;
 
 };
+
 
 
 #endif // TETRIS_H_INCLUDED

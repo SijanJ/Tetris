07871f624463f8c9d5cc0b6864f78daf4e52767a
Saphal Rimal <078bct078.saphal@pcampus.edu.np>
2023-08-04 18:37:36 +0545

    Update main.cpp
    Formatted

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -5,25 +5,28 @@
 #include<ctime>
 using namespace std;
 
+
+
 int main(int argc, char *argv[] )
 {
-srand(time(0));
-Tetris* tetris = new Tetris();
+    srand(time(0));
+    Tetris* tetris = new Tetris();
 
-const char* title = "Tetris";
-if(tetris ->init(title))
+    const char* title = "Tetris";
+    if(tetris ->init(title))
     {
-    while (tetris ->isRunning())
-    {
-        tetris-> setCurrentTime(SDL_GetTicks());
-        tetris->handleEvents();
-        tetris->gameplay();
-        tetris ->updateRender();
+        while (tetris ->isRunning())
+        {
+            tetris-> setCurrentTime(SDL_GetTicks());
+            tetris->handleEvents();
+            tetris->gameplay();
+            tetris ->updateRender();
+        }
     }
-}
-else{
-    cout << "Failed initialization";
-}
-tetris ->clean();
-return 0;
+    else
+    {
+        cout << "Failed initialization";
+    }
+    tetris ->clean();
+    return 0;
 }

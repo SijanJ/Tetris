8b279661ae850fb1c4eb9cf789155d36c8d1d1e0
YellowFlash27 <104955856+YellowFlash27@users.noreply.github.com>
2023-08-14 20:42:52 +0545

    Update powerup.h

--- tetris/powerup.h
+++ tetris/powerup.h
@@ -5,27 +5,29 @@
 
 void Tetris::powerup(int player_no, int powerup_no)
 {
+    if(total_player==1)
+        isPowerupActive[1]= false;
 if(player_no == 1)
 {
 //powerup image rendering
-setPosRect(destR, BlockW*powerup_x, BlockH*powerup_y, BlockH, BlockW);
+setPosRect(destR, BlockW*powerup_x[1], BlockH*powerup_y[1], BlockH, BlockW);
 SDL_RenderCopy(render, powerup_img, NULL, &destR);
 
 
-if(currentTime - powerupStartTime > 5000) // powerup shown for 5 seconds
-   isPowerupActive=false;
+if(currentTime[1] - powerupStartTime[1] > 5000) // powerup shown for 5 seconds
+   isPowerupActive[1]=false;
 
    for(int i=0; i<8; i++){
 
-   if(items[i].x < powerup_x + 1 && // check for collisions of powerup and block
-      items[i].x + 1 > powerup_x &&
-      items[i].y < powerup_y +1 &&
-      items[i].y + 1 > powerup_y)
+   if(items[1][i].x < powerup_x[1] + 1 && // check for collisions of powerup and block
+      items[1][i].x + 1 > powerup_x[1] &&
+      items[1][i].y < powerup_y[1] +1 &&
+      items[1][i].y + 1 > powerup_y[1])
       {
 
-        nextT =2;
-      nextTetrimino(true); //for different shape in nextTetrimino function
-     isPowerupActive=false;
+
+      nextTetrimino(2, true); //for different shape in nextTetrimino function
+     isPowerupActive[1]=false;
         //powerUp to be added soon
  }
   }
@@ -34,23 +36,22 @@ if(currentTime - powerupStartTime > 5000) // powerup shown for 5 seconds
 else // for player 2
 {
 
-setPosRect(destR, BlockW*powerup_x2, BlockH*powerup_y2, BlockH, BlockW);
+setPosRect(destR, BlockW*powerup_x[2], BlockH*powerup_y[2], BlockH, BlockW);
 SDL_RenderCopy(render, powerup_img, NULL, &destR);
 
-if(currentTime2 - powerupStartTime2> 5000)
-   isPowerupActive2=false;
+if(currentTime[2] - powerupStartTime[2]> 5000)
+   isPowerupActive[2]=false;
 
    for(int i=0; i<8; i++){
 
-   if(items2[i].x < powerup_x2+ 1 &&
-      items2[i].x + 1 > powerup_x2 &&
-      items2[i].y < powerup_y2 +1 &&
-      items2[i].y + 1 > powerup_y2)
+   if(items[2][i].x < powerup_x[2]+ 1 &&
+      items[2][i].x + 1 > powerup_x[2] &&
+      items[2][i].y < powerup_y[2] +1 &&
+      items[2][i].y + 1 > powerup_y[2])
       {
 
-      nextT =1;
-      nextTetrimino(true);
-isPowerupActive2=false;
+      nextTetrimino(1,true);
+isPowerupActive[2]=false;
       }
 
    }

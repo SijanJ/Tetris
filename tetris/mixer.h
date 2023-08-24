9e6b168453cef66aaa46b5090bea803c3b603a47
YellowFlash27 <104955856+YellowFlash27@users.noreply.github.com>
2023-08-24 12:26:49 +0545

    Update mixer.h

--- tetris/mixer.h
+++ tetris/mixer.h
@@ -1,48 +1,36 @@
 #ifndef MIXER_H_INCLUDED
 #define MIXER_H_INCLUDED
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_mixer.h>
+
 #include <iostream>
 #include <vector>
+#include "SDL.hpp"
 
-class mixer{
+class mixer:private SDL{
 private:
-    std::vector<Mix_Chunk*>sounds;
-Mix_Music *m;
+    std::vector<SoundChunk*>sounds;
+Music *m;
+int volume = 25;
     public:
-
-
-int initMixer() {
-	Mix_Init(MIX_INIT_MP3);
-
-	if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-		printf("SDL_Mixer couldnt init\n", Mix_GetError());
-
-	}
-return 0;
-
-}
-
 
 void playMusic(const char* filename)
 {
-    Mix_Music *m = NULL;
-    m = Mix_LoadMUS(filename);
+    Music *m = NULL;
+    m = loadMusic(filename);
     if(m == NULL) {
-		printf("Failed to load music\n", Mix_GetError());
+		printf("Failed to load music\n");
 
 	}
-	Mix_VolumeMusic(50);
-	Mix_PlayMusic(m, -1);
+	setVolume(volume);
+	playMUS(m, -1);
 
 }
 
 int loadSound(const char* filename) {
-	Mix_Chunk *m = NULL;
-	m = Mix_LoadWAV(filename);
+	SoundChunk *m = NULL;
+	m = loadSOUND(filename);
 	if(m == NULL) {
-		printf("Failed to load music\n", Mix_GetError());
+		printf("Failed to load music\n");
 		return -1;
 	}
 	sounds.push_back(m);
@@ -50,38 +38,38 @@ int loadSound(const char* filename) {
 }
 
 void toggleVolume() {
-	 int volume = Mix_GetMusicVolume(m);
+	 int volume = getMusicVolume(m);
           if(volume>0)
-          Mix_VolumeMusic(0);
+          setMusicVolume(0);
           else if(volume==0)
-            Mix_VolumeMusic(50);
+            setMusicVolume(25);
 }
 void rewindMusic()
 {
-    Mix_RewindMusic();
-    Mix_PauseMusic();
+    rewindMusic();
+    pauseMusic();
 }
 int playSound(int s) {
 
-        Mix_PlayChannel(-1, sounds[s], 0);
+        playSoundChunk(sounds[s],-1, 0);
 	return 0;
 }
 
 void load_AllSound(){
-int sound0 = loadSound("sound/move.ogg"); //0 = move sound
-int sound1 = loadSound("sound/harddrop.mp3"); //1= hard drop
-int sound2 = loadSound("sound/clearline.ogg");//2= line clear
-int sound3 = loadSound("sound/impact.ogg"); // 3= invalid move
-int sound4 = loadSound("sound/powerup.ogg"); // 4= powerup
+loadSound("sound/move.ogg"); //0 = move sound
+loadSound("sound/harddrop.mp3"); //1= hard drop
+loadSound("sound/clearline.ogg");//2= line clear
+loadSound("sound/impact.ogg"); // 3= invalid move
+loadSound("sound/powerup.ogg"); // 4= powerup
 }
 
 void quitMixer() {
 	for(int i=0; i<sounds.size(); i++) {
-		Mix_FreeChunk(sounds[i]);
+		freeChunk(sounds[i]);
 		sounds[i]=NULL;
 	}
 	m = NULL;
-	Mix_Quit();
+	MixerQuit();
 }
 
 
